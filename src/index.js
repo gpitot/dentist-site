2fa36c8f5684d249e0dda3e07c5a0a24764fdd42
Guillaume Pitot <guillaume.pitot@gmail.com>
2019-04-25 18:09:13 +1000

    smooth nav

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import {addServiceListeners} from './js/services';
 import {animate} from './js/intro';
-
+import {navigate} from './js/navigation';
 addServiceListeners();
 animate();
+navigate();
